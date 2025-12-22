3e10f3e108b3d81e87312efe67f91654d473ffa1
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2025-12-22 21:17:46 +0100

    enable formatting for html and css

---
+++ lua/plugins/html.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "prettier",
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["html"] = { "prettier" },
+        ["css"] = { "prettier" },
+      },
+    },
+  },
+}
