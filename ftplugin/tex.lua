78810536e17b59437c76c090b63964e1d16d723d
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2026-01-16 20:15:37 +0100

    disable concealing for latex spacing commands

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -4,3 +4,4 @@ local pdf_readers = {
 }
 
 vim.g.vimtex_view_method = pdf_readers[vim.uv.os_uname().sysname]
+vim.g.vimtex_syntax_conceal = vim.tbl_extend("force", vim.g.vimtex_syntax_conceal or {}, { spacing = 0 })
