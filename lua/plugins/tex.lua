eadff038b200d5cc4a046c947af266933f8546b2
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2026-01-16 20:15:14 +0100

    enable formatting for latex

--- lua/plugins/tex.lua
+++ lua/plugins/tex.lua
@@ -22,4 +22,12 @@ return {
       path = ".ltex",
     },
   },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["tex"] = { "latexindent" },
+      },
+    },
+  },
 }
