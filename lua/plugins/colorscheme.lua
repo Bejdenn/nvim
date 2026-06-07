0c23a48860b768c2af0a3c15bc84b435c4159c4d
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2026-06-07 22:41:59 +0200

    add detection for light/dark mode

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,22 @@
 return {
   {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      set_dark_mode = function()
+        vim.cmd([[colorscheme catppuccin-mocha]])
+        print("Switching to dark mode: ", vim.g.colors_name)
+      end,
+      set_light_mode = function()
+        vim.cmd([[colorscheme catppuccin-latte]])
+        print("Switching to light mode: ", vim.g.colors_name)
+      end,
+    },
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
