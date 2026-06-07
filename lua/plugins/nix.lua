a472f22235093272889a00abeb1b05db90a0d337
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2026-06-07 22:40:53 +0200

    setup formatting for nix files

---
+++ lua/plugins/nix.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "alejandra",
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["nix"] = { "alejandra" },
+      },
+    },
+  },
+}
